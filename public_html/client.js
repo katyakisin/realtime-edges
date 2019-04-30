f37da664714b70c1602ae41b63900321e27c614f
katherine kisin <kkisin@150-253-94-171.wifi.mcad.edu>
2019-04-30 15:41:16 -0500

    Update client.js

--- public_html/client.js
+++ public_html/client.js
@@ -121,8 +121,13 @@ function ll2poly(latlonArray, scaleFactor) {
   //push and pop so that the styles dont change anything else in the sketch and so we can cleanly translate and rotate
   push()
   //we can translate and rotate here to correct the orientation of the shape to face "north"/up
-  translate((width / 2.2) - (scalar + width / 2.2), (height / 2.2) - (scalar + height / 2.2)); //manipulate position here
-  scale(scaleFactor)
+
+  translate(width/2, height/2) //move the whole thing to the center of the screen
+  tranlate(-scalar*width/2.5), -scalar*height/2.5) //move the origin point of the shape to the center instead of the top left corner based on scale and screensize
+
+
+  // translate((width / 1.5) - (scalar + width / 2), (height / 1.5) - (scalar + height / 2)); //manipulate position here
+  // scale(scaleFactor)
   // rotate(radians(90)) //rotate by 90 degrees
 
   beginShape()
