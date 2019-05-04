1727e0c01cc05abf465d7c29652d15e3a62bf151
katherine kisin <kkisin@150-253-92-34.wifi.mcad.edu>
2019-05-04 14:45:33 -0500

    blednmode

--- public_html/client.js
+++ public_html/client.js
@@ -80,7 +80,7 @@ function setup() {
 }
 
 function draw() {
-  // blendMode(EXCLUSION);
+  blendMode(EXCLUSION);
     fill(242, 241, 232);
     stroke(0);
 }
@@ -124,10 +124,9 @@ function ll2poly(latlonArray, scaleFactor) {
   push()
   //we can translate and rotate here to correct the orientation of the shape to face "north"/up
   // translate((width / 5) - (scalar + width / 5), (height / 2) - (scalar + height / 2)); //manipulate position here
-  scale(scaleFactor)
   translate(width/2, height/2) //move the whole thing to the center of the screen
   translate(-scalar*width/2.5, -scalar*height/2.5) //move the origin point of the shape to the center instead of the top left corner
-
+  scale(scaleFactor)
   // rotate(radians(90)) //rotate by 90 degrees
 
   beginShape()
