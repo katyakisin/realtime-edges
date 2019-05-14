4f839bdc23ce39092fd4adb1146c580cfefee5dc
katherine kisin <kkisin@150-253-95-23.wifi.mcad.edu>
2019-05-14 15:20:40 -0500

    Update client.js

--- public_html/client.js
+++ public_html/client.js
@@ -48,7 +48,7 @@ socket.on('game-over', function() {
 
 // ***** START P5 STUFF *********
 
-let scalar = 0.3; //starting size
+let scalar = 0.2; //starting size
 let ringInc = 0.15;
 function setup() {
   createCanvas(windowWidth, windowHeight);
